969fb9f8bb8c51ac87e87b7e97e4de9212d81efc
subha2637 <saroj2002mandal@gmail.com>
2024-07-21 12:43:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        arrow: true,
+      items: 1,
+      loop: true,
+      margin: 10,
+      autoplay: true,
+      autoplayTimeout: 3000, // 3 seconds
+      autoplayHoverPause: true
+   
+    });
+  });
